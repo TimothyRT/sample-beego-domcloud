e26613fda23cce796ff0f6c832faedec3e2ae096
TimothyRT <timothyrtzion@gmail.com>
2026-06-22 00:02:33 +0700

    Port fix?

--- main.go
+++ main.go
@@ -8,6 +8,12 @@ import (
 )
 
 func main() {
+	if portStr := os.Getenv("PORT"); portStr != "" {
+        port, err := strconv.Atoi(portStr)
+        if err == nil {
+            web.BConfig.Listen.HTTPPort = port
+        }
+    }
 	fmt.Println("PORT =", os.Getenv("PORT"))
 	beego.Run()
 }
