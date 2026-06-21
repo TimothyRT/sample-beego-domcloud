4f0d658d8cfdc8fe5aa9df184c9ec53d83e30455
TimothyRT <timothyrtzion@gmail.com>
2026-06-22 00:05:50 +0700

    Import fix

--- main.go
+++ main.go
@@ -4,17 +4,19 @@ import (
 	"fmt"
 	"os"
 	_ "sample-beego-domcloud/routers"
+	"strconv"
+	"web"
+
 	beego "github.com/beego/beego/v2/server/web"
 )
 
 func main() {
 	if portStr := os.Getenv("PORT"); portStr != "" {
-        port, err := strconv.Atoi(portStr)
-        if err == nil {
-            web.BConfig.Listen.HTTPPort = port
-        }
-    }
+		port, err := strconv.Atoi(portStr)
+		if err == nil {
+			web.BConfig.Listen.HTTPPort = port
+		}
+	}
 	fmt.Println("PORT =", os.Getenv("PORT"))
 	beego.Run()
 }
-
